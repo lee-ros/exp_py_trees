9e548751a58af6e36f53e997811a71d0664cc9a4
Lee Rosenberg <laluk321@gmail.com>
2023-02-22 12:55:05 +0200

    add launch file

---
+++ src/launch/system_state_launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='system_behaviours_manager',
+            executable='system_state_mocker',
+            name='system_state_mocker',
+            remappings=[
+                ('~/system_state', '/system_state')
+            ],
+        ),
+        Node(
+            package='system_behaviours_manager',
+            executable='system_behaviour_tree',
+            name='system_behaviour_tree',
+            emulate_tty=True,
+            output='screen',
+            remappings=[
+                ('~/system_state', '/system_state')
+            ],
+        )
+    ])
